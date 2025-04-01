08cd1e4b3bbcda0536b9d3cd136675e3aae17bb6
Nitish <mail.logicninjax@gmail.com>
2025-04-01 20:06:04 +0530

    add rest controllers

--- src/main/java/com/example/blogging_api/repository/BlogRepository.java
+++ src/main/java/com/example/blogging_api/repository/BlogRepository.java
@@ -2,8 +2,12 @@ package com.example.blogging_api.repository;
 
 import com.example.blogging_api.entity.Blog;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 
 public interface BlogRepository extends MongoRepository<Blog, ObjectId> {
 
+    List<Blog> findByAuthor(Pageable pageable, String author);
 }
--- src/main/java/com/example/blogging_api/repository/UserRepository.java
+++ src/main/java/com/example/blogging_api/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.blogging_api.repository;
 
 import com.example.blogging_api.entity.User;
+import com.example.blogging_api.exception.UserNotFoundException;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
 
-    Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username) throws UserNotFoundException;
 }
