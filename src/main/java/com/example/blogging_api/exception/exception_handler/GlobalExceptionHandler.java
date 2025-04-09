6565a710a3b3fd6f0a1eff5333b783c9fa6eb829
Nitish <mail.logicninjax@gmail.com>
2025-04-09 14:07:48 +0530

    add method to handle bean validation exceptions

---
+++ src/main/java/com/example/blogging_api/exception/exception_handler/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.blogging_api.exception.exception_handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
