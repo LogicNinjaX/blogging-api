8d393f39923ac6564b9bc632a23122236b390cc6
Nitish <mail.logicninjax@gmail.com>
2025-04-09 14:09:40 +0530

    add exception handler method to handle invalid requests

--- src/main/java/com/example/blogging_api/exception/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/blogging_api/exception/exception_handler/GlobalExceptionHandler.java
@@ -1,9 +1,33 @@
 package com.example.blogging_api.exception.exception_handler;
 
+import com.example.blogging_api.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse<Map<String, String>>> validationHandler(MethodArgumentNotValidException ex){
+
+        Map<String, String> errors = new LinkedHashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse<>(false, "invalid request", HttpStatus.BAD_REQUEST.value(), errors));
+    }
 }
