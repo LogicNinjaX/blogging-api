edccd4174d2f191cd58ae24e4ad34888c20f017c
Nitish <mail.logicninjax@gmail.com>
2025-04-12 22:56:17 +0530

    add logging

--- src/main/java/com/example/blogging_api/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/blogging_api/service/impl/UserServiceImpl.java
@@ -5,11 +5,15 @@ import com.example.blogging_api.entity.User;
 import com.example.blogging_api.repository.UserRepository;
 import com.example.blogging_api.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private UserRepository userRepository;
     private ModelMapper modelMapper;
@@ -24,6 +28,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        logger.info("user details saved");
         return modelMapper.map(userRepository.save(user), UserDto.class);
     }
 }
