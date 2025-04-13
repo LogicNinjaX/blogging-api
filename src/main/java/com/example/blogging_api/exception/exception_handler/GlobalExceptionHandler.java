25deff594175b9e903ac84a518414ca0edd68c69
Nitish <mail.logicninjax@gmail.com>
2025-04-13 22:27:07 +0530

    add exception handler for post

--- src/main/java/com/example/blogging_api/exception/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/blogging_api/exception/exception_handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.blogging_api.exception.exception_handler;
 
+import com.example.blogging_api.dto.ApiResponse;
 import com.example.blogging_api.dto.ErrorResponse;
+import com.example.blogging_api.exception.PostNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -29,5 +31,12 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse<>(false, "invalid request", HttpStatus.BAD_REQUEST.value(), errors));
+    }
+
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<ErrorResponse<?>> postHandler(PostNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse<>(false, ex.getMessage(), HttpStatus.NOT_FOUND.value()));
     }
 }
